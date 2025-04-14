0d5531992d205e21f0d6029cca77a91fbe28071a
root <root@Mirmikas>
2025-04-14 22:09:12 +0200

    Bind express to 0.0.0.0 to allow external access in k8s

--- restauranty/backend/auth/server.js
+++ restauranty/backend/auth/server.js
@@ -3,6 +3,6 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 6001
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on http://0.0.0.0:${PORT}`);
 });

32fa17283e0abf268956a5dc6781b1b9a1700c8a
root <root@Mirmikas>
2025-04-15 15:56:58 +0200

    Change the listening part in  server.js files to localhost

--- restauranty/backend/auth/server.js
+++ restauranty/backend/auth/server.js
@@ -3,6 +3,7 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 6001
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
+
